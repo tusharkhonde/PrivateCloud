31b6daa331c60fb16b7b1090463e320ca74d7ab5
Tushar Khonde <tusharskhonde@gmail.com>
2015-05-21 13:57:47 -0700

    Commit

---
+++ src/main/java/CONFIG/SJSULAB.java
@@ -0,0 +1,17 @@
+package CONFIG;
+
+public class SJSULAB
+{
+    
+    public static String getVCenterURL() { return "https://130.65.132.103/sdk" ; }
+    public static String getVCenterLogin() { return "administrator" ; }
+    public static String getVCenterPassword() { return "12!@qwQW" ; }
+
+    public static String getVmwareHostURL() { return "https://130.65.132.19/sdk" ; }
+    public static String getVmwareLogin() { return "student@vsphere.local" ; }
+    public static String getVmwarePassword() { return "12!@qwQW" ; }
+
+
+        
+    
+}
